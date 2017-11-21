8d3777b7aec90c29f4c8cf642c9e84da7e6a9626
bubbleboy14 <mario.balibrera@gmail.com>
2017-11-21 00:34:45 -0800

    reorg: cronswarm -> cronswarmdb; cronbalance -> cronswarm.

--- ctswarm/cronswarm.py
+++ ctswarm/cronswarm.py
@@ -1,23 +1,19 @@
-from datetime import datetime, timedelta
-from cantools.scripts.migrate import load
+from dez.http.reverseproxy import startreverseproxy
 from cantools.web import respond
 from cantools.util import log
 from cantools import config
-from model import *
 
 def response():
-	log("initiating cronswarm", important=True)
-	cutoff = datetime.now() - timedelta(seconds=config.ctswarm.interval)
-	if config.ctswarm.peers:
-		for modname, schema in db.get_schema().items():
-			if "modified" in schema:
-				for (host, port) in config.ctswarm.peers.map(lambda x : x.split(".")):
-					load(host, port, db.session, {
-						"modified": {
-							"value": cutoff,
-							"comparator": ">="
-						}
-					})
-	log("cronswarm complete")
+	log("initiating cronbalance", important=True)
+	log("checking for load balancer configuration...")
+	options = config.ctswarm.revolver
+	if options.cfg:
+		log("initializing load balancer with config: %s"%(options.cfg))
+		options.update("override_redirect", not options.redirect)
+		startreverseproxy(options, False)
+		if options.cert:
+			log("starting SSL redirect (80->443)")
+			startreverseproxy(options.sslredir, False)
+	log("load balancer initialized")
 
 respond(response)
---
+++ ctswarm/cronswarmdb.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+from cantools.scripts.migrate import load
+from cantools.web import respond
+from cantools.util import log
+from cantools import config
+from model import *
+
+def response():
+	log("initiating cronswarm", important=True)
+	cutoff = datetime.now() - timedelta(seconds=config.ctswarm.interval)
+	if config.ctswarm.peers:
+		for modname, schema in db.get_schema().items():
+			if "modified" in schema:
+				for (host, port) in config.ctswarm.peers.map(lambda x : x.split(".")):
+					load(host, port, db.session, {
+						"modified": {
+							"value": cutoff,
+							"comparator": ">="
+						}
+					})
+	log("cronswarm complete")
+
+respond(response)
