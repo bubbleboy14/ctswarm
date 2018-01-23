0520ef89e0d2c8d0e0b95d19284997a214df3b39
bubbleboy14 <mario.balibrera@gmail.com>
2018-01-22 21:10:08 -0800

    uptime monitor

---
+++ ctswarm/cronswarmup.py
@@ -0,0 +1,36 @@
+from cantools.web import respond, fetch, mail
+from cantools.util import log
+from cantools import config
+
+TMP = """Hi there!
+
+Looks like these servers aren't running:
+
+%s
+
+Oops!
+"""
+
+def response():
+	log("cronswarm (up)", important=True)
+	down = []
+	for addr in config.ctswarm.monitor:
+		proto = "http"
+		host = addr
+		port = 80
+		if "://" in addr:
+			proto, host = addr.split("://")
+		if ":" in host:
+			host, port = host.split(":")
+		try:
+			fetch(host, port=port, protocol=proto)
+		except:
+			log("SERVER DOWN: %s"%(addr,))
+			down.append(addr)
+	if down:
+		dz = "\r\n".join(down)
+		for contact in config.admin.contacts:
+			mail(to=contact, subject="Server(s) Unreachable", body=TMP%(dz,))
+	log("cronswarm (up) complete")
+
+respond(response)
