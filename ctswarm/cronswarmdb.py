4ccc0113476a4a58a4796d44cd37dfdb287b1bac
bubbleboy14 <mario.balibrera@gmail.com>
2018-04-09 04:01:51 -0700

    db: use load_model() instead of load(); basic prettifactor

--- ctswarm/cronswarmdb.py
+++ ctswarm/cronswarmdb.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from cantools.scripts.migrate import load
+from cantools.scripts.migrate import load_model
 from cantools.web import respond
 from cantools.util import log
 from cantools import config
@@ -7,17 +7,23 @@ from model import *
 
 def response():
 	log("cronswarm (db)", important=True)
-	cutoff = datetime.now() - timedelta(seconds=int(config.ctswarm.db.interval))
 	if config.ctswarm.db.peers:
+		cutoff = {
+			"value": datetime.now() - timedelta(seconds=int(config.ctswarm.db.interval)),
+			"comparator": ">="
+		}
+		peers = map(lambda x : x.split(":"), config.ctswarm.db.peers.split("|"))
 		for modname, schema in db.get_schema().items():
+			filters = {}
 			if "modified" in schema:
-				for (host, port) in map(lambda x : x.split(":"), config.ctswarm.db.peers.split("|")):
-					load(host, port, db.session, {
-						"modified": {
-							"value": cutoff,
-							"comparator": ">="
-						}
-					}, config.cache("remote admin password? "))
+				filters["modified"] = cutoff
+			elif "date" in schema:
+				filters["date"] = cutoff
+			else:
+				continue
+			for (host, port) in peers:
+				load_model(modname, host, port, db.session, filters,
+					config.cache("remote admin password? "))
 	log("cronswarm (db) complete")
 
 respond(response)
