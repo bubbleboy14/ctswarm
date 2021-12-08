d093f9411722818fc91eb6128a8b7e4737a95042
bubbleboy14 <mario.balibrera@gmail.com>
2021-12-07 19:25:32 -0800

    util.snap(): replace spaces w/ underscores in archive filenames

--- ctswarm/util.py
+++ ctswarm/util.py
@@ -12,7 +12,7 @@ def snap():
 	if match:
 		log("matching archive: %s - aborting snap"%(match,))
 		return match
-	aname = str(datetime.now()).rsplit(":", 1)[0]
+	aname = str(datetime.now()).rsplit(":", 1)[0].replace(" ", "_")
 	cmd('cp data.db "%s"'%(os.path.join("archive", aname),))
 	return aname
 
