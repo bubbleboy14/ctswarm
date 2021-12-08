a00868df22cc51bf80c1fd3a3e29ab32cd199353
bubbleboy14 <mario.balibrera@gmail.com>
2021-12-07 19:09:39 -0800

    util w/ snap()/swap()

---
+++ ctswarm/util.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from cantools.util import log, cmd, read, indir
+
+def snap():
+	log("attempting snap", important=True)
+	if not os.path.exists("archive"):
+		log("creating archive directory")
+		os.mkdir("archive")
+	data = read("data.db", binary=True)
+	match = indir(data, "archive")
+	if match:
+		log("matching archive: %s - aborting snap"%(match,))
+		return match
+	aname = str(datetime.now()).rsplit(":", 1)[0]
+	cmd('cp data.db "%s"'%(os.path.join("archive", aname),))
+	return aname
+
+def swap(archname):
+	snap()
+	cmd('cp "%s" data.db'%(os.path.join("archive", archname),))
