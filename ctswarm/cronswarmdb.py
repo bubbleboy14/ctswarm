43bf491bba8dd15b77020f37bc478c861d34d842
bubbleboy14 <mario.balibrera@gmail.com>
2023-11-03 15:09:41 -0700

    adjusted log()ging to include backlog tracking

--- ctswarm/cronswarmdb.py
+++ ctswarm/cronswarmdb.py
@@ -26,34 +26,35 @@ else:
 blobifier = blobificator(config.web.host, config.web.port, dbcfg.self)
 
 def response():
-	log("cronswarm (db)", important=True)
-	if dbcfg.peers:
-		dbcfg.update("backlog", dbcfg.get("backlog", 0) + dbcfg.interval)
-		pw = config.cache("remote admin password? ")
-		delivered = 0
-		cutoff = {
-			"value": datetime.now() - timedelta(seconds=dbcfg.backlog),
-			"comparator": ">="
-		}
-		for modname, schema in schemas:
-			filters = {}
-			if "modified" in schema:
-				filters["modified"] = cutoff
-			elif "date" in schema:
-				filters["date"] = cutoff
-			else:
-				continue
-			for (host, port, protocol) in dbcfg.peers:
-				delivered += load_model(modname, host, port, db.session,
-					filters, protocol, pw, "edits", blobifier)
-		if delivered:
-			log("delivered %s updates - clearing peer memcaches"%(delivered,))
-			for (host, port, protocol) in dbcfg.peers:
-				post(host, "/_memcache", port, {
-					"pw": pw,
-					"action": "clear"
-				}, protocol=protocol)
-		dbcfg.update("backlog", 0)
+	if not dbcfg.peers:
+		return log("cronswarm (db) aborted - no peers")
+	dbcfg.update("backlog", dbcfg.get("backlog", 0) + dbcfg.interval)
+	log("cronswarm (db) @ %s"%(dbcfg.backlog,), important=True)
+	pw = config.cache("remote admin password? ")
+	delivered = 0
+	cutoff = {
+		"value": datetime.now() - timedelta(seconds=dbcfg.backlog),
+		"comparator": ">="
+	}
+	for modname, schema in schemas:
+		filters = {}
+		if "modified" in schema:
+			filters["modified"] = cutoff
+		elif "date" in schema:
+			filters["date"] = cutoff
+		else:
+			continue
+		for (host, port, protocol) in dbcfg.peers:
+			delivered += load_model(modname, host, port, db.session,
+				filters, protocol, pw, "edits", blobifier)
+	if delivered:
+		log("delivered %s updates - clearing peer memcaches"%(delivered,))
+		for (host, port, protocol) in dbcfg.peers:
+			post(host, "/_memcache", port, {
+				"pw": pw,
+				"action": "clear"
+			}, protocol=protocol)
+	dbcfg.update("backlog", 0)
 	log("cronswarm (db) complete")
 
 respond(response)
